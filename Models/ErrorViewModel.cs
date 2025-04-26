8b906ec703634857b4601e48ac03eb33897cda0a
Megan J <156514833+megantia@users.noreply.github.com>
2025-04-26 15:14:58 +0200

    Initial file upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Participation3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
